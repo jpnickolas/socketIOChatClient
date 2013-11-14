e35e7a82707a69f6e5567768b20760f7827b0d70
Nick Hilton <hilton@csh.rit.edu>
2013-11-14 14:04:59 -0500

    Changed socketDemo.js to contain more helpful comments. Next, it now escapes the < and > characters so that the users cannot inject html Also updated the index file to look prettier.

--- socketDemo.js
+++ socketDemo.js
@@ -1,17 +1,24 @@
 /**
- * Tests socket.io by creating a small chat client through telnet
+ * Tests socket.io by creating a small chat client through http
  * Nick Hilton
  */
 
-//Include the server
+//Include a file stream to read out a file
 var fs = require('fs');
-var http = require('http').createServer(function(request, response) {
+
+//Includes and initializes the server
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
 	
+	//Reads out an html file
 	fs.readFile(__dirname+'/index.html', function(err, data) {
 		if(err) {
 			response.writeHead(500);
 			respoonse.end("Error loading index.html");
 		}
+
+		//Writes the file out to the response stream
 		else {
 			response.writeHead(200);
 			response.end(data);
@@ -19,29 +26,48 @@ var http = require('http').createServer(function(request, response) {
 	});
 });
 
+//opens up a log file
+fs.openSync('log.txt','w');
 
-var io = require('socket.io').listen(http);
+fs.appendFile('log.txt', 'Log Start');
 
-http.listen(8080);
+//includes the sockets, and tells them to listen on the http server
+var io = require('socket.io').listen(server);
 
+//initializes the socket handler to happen on connection
 io.sockets.on('connection', function(socket) {
-	
-	console.log('Fresh meat!');
 
+	//Starts by asking the user for their username
 	socket.emit('message', {message:"Enter your username: "});
-			
+	
+	//stores the username
 	var username = null;
 	
+	//occurs whenever the socket receives a message from the user
 	socket.on('message', function(data) {
+		data.message = data.message.replace(/</ig,"&lt;");
+		data.message = data.message.replace(/>/ig,"&gt;");
+
+		//checks if it is receiving the username
 		if(username==null) {
 			username=data.message;
+			fs.appendFile('log.txt',username+" has connected!\n");
 			io.sockets.emit('message', {message:username+" has connected!"});
 		}
-		else
+		else {
+			fs.appendFile('log.txt', username+": "+data.message+"\n");
 			io.sockets.emit('message',{message:username+": "+data.message+"\n"});
+		}
 	});
 	
+	//event occurs on disconnect
 	socket.on('disconnect', function() {
-		io.sockets.emit('message',{message:username+" disconnected!\n"});
+		if(username!=null) {
+			fs.appendFile('log.txt',username+" disconnected\n");
+			io.sockets.emit('message',{message:username+" disconnected!\n"});
+		}
 	});
 });
+
+//has the server listen on port 80
+server.listen(80);
